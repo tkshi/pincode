ad97726b37a75e74f1cbf15f2a8f29088d58929c
tkshi <takashi@endoutakashi-no-MacBook-Air.local>
2016-03-06 16:36:59 +0900

    viewDataFromFirebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import Firebase from 'firebase';
+
+let ref = new Firebase('https://pincode.firebaseio.com/');
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 1};
+  }
   render() {
     return (
       <div>
          <Button bsSize="large">Large button</Button>
-        <h1>hello</h1>
+        <h1>hello{this.state.count}</h1>
       </div>
     );
   }
