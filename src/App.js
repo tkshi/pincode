dacffe78276bd3fa3b183de5644434742e9e9a2c
tkshi <takashi@endoutakashi-no-MacBook-Air.local>
2016-03-06 17:42:38 +0900

    close #3

--- src/App.js
+++ src/App.js
@@ -9,11 +9,26 @@ export class App extends Component {
     super(props);
     this.state = {count: 1};
   }
+  componentWillMount() {
+    ref.child('pins/1').on('value',(snap)=>{
+      this.setState({title:snap.val().title})
+    })
+  }
+  submitData() {
+    ref.child('pins').push({
+      title:"hi",
+      mode:"markdown",
+      code:this.refs.code.value
+    })
+  }
   render() {
     return (
       <div>
          <Button bsSize="large">Large button</Button>
         <h1>hello{this.state.count}</h1>
+        <h1>{this.state.title}</h1>
+        <input type='text' ref='code'></input>
+        <button onClick={this.submitData.bind(this)}>submit</button>
       </div>
     );
   }
