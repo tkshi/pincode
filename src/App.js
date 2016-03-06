896beeaa297065511aba466ecbb20106b6bcd458
tkshi <takashi@endoutakashi-no-MacBook-Air.local>
2016-03-06 17:55:45 +0900

    close #4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Firebase from 'firebase';
+var Codemirror = require('react-codemirror');
+require('codemirror/mode/javascript/javascript');
 
 let ref = new Firebase('https://pincode.firebaseio.com/');
 
+var Editor = React.createClass({
+    getInitialState: function() {
+        return {
+            code: "// Code",
+            mode:'javascript'
+        };
+    },
+    updateCode: function(newCode) {
+        this.setState({
+            code: newCode
+        });
+    },
+    render: function() {
+        var options = {
+            lineNumbers: true
+        };
+        return <Codemirror value={this.state.code} onChange={this.updateCode} options={options} />
+    }
+});
+
 export class App extends Component {
   constructor(props) {
+    console.log(Codemirror)
     super(props);
     this.state = {count: 1};
   }
@@ -29,6 +52,7 @@ export class App extends Component {
         <h1>{this.state.title}</h1>
         <input type='text' ref='code'></input>
         <button onClick={this.submitData.bind(this)}>submit</button>
+        <Editor></Editor>
       </div>
     );
   }
