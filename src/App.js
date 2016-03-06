157a63d2018c9066b97bddbc745a97ad09396a2a
tkshi <takashi@endoutakashi-no-MacBook-Air.local>
2016-03-06 15:59:26 +0900

    close #1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 export class App extends Component {
   render() {
     return (
       <div>
+         <Button bsSize="large">Large button</Button>
         <h1>hello</h1>
       </div>
     );
